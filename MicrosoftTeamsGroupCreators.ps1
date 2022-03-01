88b31dfe40201fc6c94eaac3e83747597cd7c810
Mehmet PARLAKYİĞİT <53214224+mparlakyigit@users.noreply.github.com>
2022-03-01 11:01:17 +0300

    Add files via upload

---
+++ MicrosoftTeamsGroupCreators.ps1
@@ -0,0 +1,47 @@
+﻿#Kullanıcıların Microsoft Teams Üzerinde takım oluşturmasını engelleme
+#https://www.parlakyigit.net/microsoft-teams-kullanicilarinin-takim-olusturmasini-engelleyin/
+
+
+Install-Module -Name ExchangeOnlineManagement
+Set-ExecutionPolicy Unrestricted
+Get-ExecutionPolicy
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline
+
+
+New-DistributionGroup -Name "Teams Admins" -Alias TeamsAdmins -DisplayName "Teams Admins" -Type Security -Members @("Test One","Test Two")
+
+
+#Code Source https://docs.microsoft.com/en-us/microsoft-365/solutions/manage-creation-of-groups?view=o365-worldwide#step-2-run-powershell-commands
+
+Uninstall-Module AzureAD
+Install-Module AzureADPreview
+
+
+#Code Source https://docs.microsoft.com/en-us/microsoft-365/solutions/manage-creation-of-groups?view=o365-worldwide#step-2-run-powershell-commands
+$GroupName = "Teams Admins"
+$AllowGroupCreation = $False
+
+Connect-AzureAD
+
+$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+if(!$settingsObjectID)
+{
+    $template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
+    $settingsCopy = $template.CreateDirectorySetting()
+    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+}
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+if($GroupName)
+{
+  $settingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
+} else {
+$settingsCopy["GroupCreationAllowedGroupId"] = $GroupName
+}
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
+
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
